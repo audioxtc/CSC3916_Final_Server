2553b5c44bb0fa2bde969d70564c4a5d72aa80f3
Chris Frey <christopher.r.frey1@gmail.com>
2021-03-22 20:57:54 -0600

    init

---
+++ config/config.js
@@ -0,0 +1,16 @@
+const config = {
+  env: process.env.NODE_ENV || 'development',
+  port: process.env.PORT || 3000,
+  jwtSecret: process.env.JWT_SECRET || "YOUR_secret_key",
+  mongoUri: 'mongodb+srv://chrispyfrey:goblins@cluster0.fu88u.mongodb.net/Cluster0?retryWrites=true&w=majority', 
+  /*process.env.MONGODB_URI ||
+    process.env.MONGO_HOST ||
+    'mongodb://' + (process.env.IP || 'localhost') + ':' +
+    (process.env.MONGO_PORT || '27017') +
+    '/mernproject',*/
+  stripe_connect_test_client_id: 'YOUR_stripe_connect_test_client',
+  stripe_test_secret_key: 'YOUR_stripe_test_secret_key',
+  stripe_test_api_key: 'YOUR_stripe_test_api_key'
+}
+
+module.exports = config;
