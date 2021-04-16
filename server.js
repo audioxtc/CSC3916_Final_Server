ab36f726d9d81813936deef235e159400c70af9b
Chris Frey <christopher.r.frey1@gmail.com>
2021-04-15 19:16:37 -0600

    Added arguments to mongoose.connect to clear warnings

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var mongoose = require('mongoose');
 var config = require('./config/config');
 
 // Connection URL
-mongoose.connect(config.mongoUri, { autoIndex: false });
+mongoose.connect(config.mongoUri, { autoIndex: false, useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.connection.on('error', () => {
   throw new Error(`unable to connect to database: ${mongoUri}`)
 });
